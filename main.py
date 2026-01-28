b70ffdd93454f543936711d0b83889a0931d42e8
nyb <nyb395@gmail.com>
2026-01-28 17:00:46 +0200

    changed error handling to function

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ network = SpaceNetwork(2)
 sat1 = Satellite("sat1",100)
 sat2 = Satellite("sat2",200)
 packet1 = Packet("hello my friend", sat1, sat2)
-while True:
-    try:
-        network.send(packet1)
-    except (TemporalInterferenceError, DataCorruptedError) as e:
-        if isinstance(e, TemporalInterferenceError):
-            time.sleep(2)
-            continue
-        if isinstance(e, DataCorruptedError):
-            continue
-    break
+def attempt_transmission(packet):
+    while True:
+        try:
+            network.send(packet)
+        except (TemporalInterferenceError, DataCorruptedError) as e:
+            if isinstance(e, TemporalInterferenceError):
+                print("Interference, waiting...")
+                time.sleep(2)
+                continue
+            if isinstance(e, DataCorruptedError):
+                print("Data corrupted, retrying...")
+                continue
+        break
+attempt_transmission(packet1)
