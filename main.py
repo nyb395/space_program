258f1bbec66ae147f711795e3520c5cb3129125b
nyb <nyb395@gmail.com>
2026-01-28 11:50:28 +0200

    removed satellite, add integration to satellite file

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
+from satellite import Satellite
 from space_network_lib import SpaceEntity, SpaceNetwork, Packet
 
-
-class Satellite(SpaceEntity):
-    def __init__(self, name, distance_from_earth):
-        super().__init__(name, distance_from_earth)
-
-    def receive_signal(self, packet: Packet):
-        print(f"[{self.name}] Received: {packet}")
-
-
-network = SpaceNetwork(1)
+network = SpaceNetwork(2)
 sat1 = Satellite("sat1",100)
 sat2 = Satellite("sat2",200)
 packet1 = Packet("hello my friend", sat1, sat2)
