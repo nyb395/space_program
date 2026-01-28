cad9d99b03eecb6ec0dd31b092ae68af2d36c1f2
nyb <nyb395@gmail.com>
2026-01-28 12:24:03 +0200

    add error handling

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from satellite import Satellite
-from space_network_lib import SpaceEntity, SpaceNetwork, Packet
+from space_network_lib import SpaceNetwork, Packet, TemporalInterferenceError, DataCorruptedError
+import time
 
 network = SpaceNetwork(2)
 sat1 = Satellite("sat1",100)
 sat2 = Satellite("sat2",200)
 packet1 = Packet("hello my friend", sat1, sat2)
-network.send(packet1)
-
+while True:
+    try:
+        network.send(packet1)
+    except (TemporalInterferenceError, DataCorruptedError) as e:
+        if isinstance(e, TemporalInterferenceError):
+            time.sleep(2)
+            continue
+        if isinstance(e, DataCorruptedError):
+            continue
+    break
