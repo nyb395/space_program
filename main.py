d6e2ea84d039e8a1972035bd0e6132793637855c
nyb <nyb395@gmail.com>
2026-01-29 14:56:46 +0200

    implemented comms_error_handling.py

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
 from satellite import Satellite
-from space_network_lib import SpaceNetwork, Packet, TemporalInterferenceError, DataCorruptedError
-import time
+from space_network_lib import SpaceNetwork, Packet
+from comms_error_handling import attempt_transmission
 
 network = SpaceNetwork(2)
 sat1 = Satellite("sat1",100)
 sat2 = Satellite("sat2",200)
 packet1 = Packet("hello my friend", sat1, sat2)
-def attempt_transmission(packet):
-    while True:
-        try:
-            network.send(packet)
-        except (TemporalInterferenceError, DataCorruptedError) as e:
-            if isinstance(e, TemporalInterferenceError):
-                print("Interference, waiting...")
-                time.sleep(2)
-                continue
-            if isinstance(e, DataCorruptedError):
-                print("Data corrupted, retrying...")
-                continue
-        break
-attempt_transmission(packet1)
+attempt_transmission(packet1, network)
