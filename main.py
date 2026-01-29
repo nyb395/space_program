2275435b1ebe19c7b65c36cc232b16eedde95dd2
nyb <nyb395@gmail.com>
2026-01-29 15:40:08 +0200

    level up to 3

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from satellite import Satellite
 from space_network_lib import SpaceNetwork, Packet
 from comms_error_handling import attempt_transmission
 
-network = SpaceNetwork(2)
+network = SpaceNetwork(3)
 sat1 = Satellite("sat1",100)
 sat2 = Satellite("sat2",200)
 packet1 = Packet("hello my friend", sat1, sat2)
